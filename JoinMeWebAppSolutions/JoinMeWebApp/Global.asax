7f74d5262332c46b907c9b7a42d6e5e495c41d98
thordis14 <thordis14@ru.is>
2015-04-29 22:13:41 +0000

    Created Visual Studio Project

---
+++ JoinMeWebAppSolutions/JoinMeWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JoinMeWebApp.MvcApplication" Language="C#" %>
---
+++ JoinMeWebAppSolutions/JoinMeWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JoinMeWebApp.Startup))]
+namespace JoinMeWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
